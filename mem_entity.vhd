d1cd95d811febc1e62381c1e20381f319164fcf0
wildevn <45374307+wildevn@users.noreply.github.com>
2022-10-10 00:44:47 -0400

    Update mem_entity.vhd
    trying to create a type. But there's some issues related to position and conversion types.

--- mem_entity.vhd
+++ mem_entity.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity memory is
     generic(
@@ -13,22 +14,40 @@ port(
     data_write: in std_logic; -- When '1', write data to memory
  -- Data address given to memory
     data_addr : in std_logic_vector(addr_width-1 downto 0);
-    
  -- Data sent from memory when data_read = '1' and data_write = '0'
     data_in : in std_logic_vector(data_width-1 downto 0);
-
  -- Data sent to memory when data_read = '0' and data_write = '1'
     data_out : out std_logic_vector((data_width*4)-1 downto 0)
 );
 end entity;
 
 architecture behavioral of memory is
+    -- create a new type named something near signal little_data_vet : std_logic_vector(data_width - 1 down to 0);
+    -- create a new variable data_vet : little_data_vet type (addr_width - 1 downto 0);
+    -- aparently it doesn't need to be first a type because we're
+    -- declaring a mult dimensional vector. hope it works
+    -- needs to change the address range (first dimension)
+    -- type data_vet_t is array ((2**data_width) downto 0, data_width - 1 downto 0) of std_logic;
+    type data_vet_t is array (data_width - 1 downto 0) of std_logic;
+    type dt_vet_t is array((2**data_width) downto 0) of data_vet_t;
+    signal data_vet : dt_vet_t := (others => 0);
+    signal a : std_logic;
 begin
-    -- seta o endereço de memoria apontado por ele
-    -- data_addr 
-    -- read(arq_load, data_in(i))
-    -- write()
-    -- file_open(read_mode)
-
-    data_out <= data_in when data_read = '0' and data_write = '1' and falling_edge(clock);
+    read_d: process (data_read) is
+        begin
+            if data_read = '1' and data_write = '0' then
+                data_out((data_width*4)-1 downto 0) <= data_vet((signed(data_addr)+3) downto signed(data_addr'value));
+            else
+                a <= '0';     
+            end if;
+        end process;
+    write_d: process (data_write) is
+        begin
+            if data_read = '0' and data_write = '1' and falling_edge(clock) then
+                data_vet(signed(data_addr)) <= data_in;
+                data_out((data_width*4)-1 downto 0) <= data_vet((signed(data_addr)+3) downto signed(data_addr));
+            else
+                a <= '0';
+            end if;
+        end process;
 end architecture;
