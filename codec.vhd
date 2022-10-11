8b0b19adfa7ff1b1849eecc641be233f6b98503a
LinaxT25 <viniciusgalvao2000@gmail.com>
2022-10-10 22:18:15 -0400

    Reading files

--- codec.vhd
+++ codec.vhd
@@ -17,15 +17,29 @@ entity codec is
 end entity;
 
 architecture behavioral of codec is
+    type vet_int is array(7 downto 0) of integer;
+    
+    impure function read_array(nome_arq : string)
+        return vet_int is
+            type t_arq is file of integer;
+            file arq_int : t_arq open read_mode is nome_arq;
+            variable result : vet_int := (others => 0);
+            variable i : natural := 1;
+        begin
+            while not endfile(arq_int) and i <= 8 loop
+                read(arq_int, result(i));
+                i := i + 1;
+            end loop;
+            return result;
+    end function read_array;
 
 begin
     archive_inout: process (interrupt)
+        variable dados : vet_int;
     begin
-
-
-
-
-
-        valid <= true;   
+        if read_signal = '1' and write_signal = '0' then
+           dados := read_array("dados.dat");
+        end if;
     end process;
+    valid <= '1'; 
 end architecture;
