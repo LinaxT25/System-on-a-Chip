34f44dd64de5ceeeb83912db9109703757a45799
LinaxT25 <viniciusgalvao2000@gmail.com>
2022-11-13 23:54:02 -0400

    minor changes

--- cpu.vhd
+++ cpu.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity cpu is
     generic(
@@ -54,9 +55,14 @@ begin
     -- E então começar a executar as instruçoes.
     start: process(clock, codec_valid)
     begin
-        if(codec_valid = '1' and rising_edge(clock)) then
-            IP_aux <= IP_aux + 1;
+    -- tem que ser verificado se funcionam os dois ao mesmo tempo, pode ser que um nao consiga alcancer a 'velocidade'
+    -- do outro, o que implica em fazer algum wait, cabivel só em test_bench
+        if(codec_valid = '1' and rising_edge(clock)) then 
+            IP_aux <= std_logic_vector(unsigned(to_integer(unsigned(IP_aux)) + 1)); 
+            IP <= IP_aux;
         elsif(codec_valid = '0' and rising_edge(clock)) then
+            IP_aux <= std_logic_vector(unsigned(to_integer(unsigned(IP_aux)) - 1));
+            IP <= IP_aux;
             setup_finished <= true;
         end if;
     end process;
@@ -66,10 +72,7 @@ begin
     begin
         if(setup_finished = true) then
             instruction_aux(3 downto 0) :=  instruction_in(data_width - 1 downto data_width - 4);
-
-            if (inner_halt /= '1' or halt = '0') then
-                inner_halt <= '0';
-
+            if (inner_halt /= '1' or halt = '0') then -- verificar essa condicao depois
                 case instruction_aux is
                     -- HLT 0
                     when "0000" => 
@@ -80,14 +83,15 @@ begin
                         codec_read <= '1';
                         wait on codec_valid; 
                         data_in <= codec_data_out;
-                        -- att SP 
+                        SP_aux <= std_logic_vector(unsigned(integer(unsigned(SP_aux)) + 1));
+                        SP <= SP_aux; 
                         data_write <= '1';
                         data_read <= '0';
                     -- OUT 2
                     when "0010" => 
                         data_read <= '1';
-                        data_write <= '0';
-                        -- att SP
+                        data_write <= '0'; -- aqui o procedimento é mais fino, precisa saber quando se o data_out já 
+                        -- att SP           -- contem quem é pra ser escrito, as vezes n precisa ser lido pois ja esta no vetor
                         codec_data_in <= data_out(7 downto 0);
                         codec_write <= '1';
                         codec_read <= '0';
@@ -99,9 +103,10 @@ begin
                     when "0100" => 
                         data_in(7 downto 4) <= "0000";
                         data_in(3 downto 0) <= instruction_in(3 downto 0);
+                        SP_aux <= std_logic_vector(unsigned(integer(unsigned(SP_aux)) + 1));
+                        SP <= SP_aux;
                         data_write <= '1';
                         data_read <= '0';
-                        -- att SP
                     -- DROP 5
                     when "0101" => 
                         --IP_aux
@@ -111,24 +116,40 @@ begin
                     when "0110" => 
                     -- ADD 8
                     when "1000" => 
+                        --data_in <= codec_data_out + data_out;
                     -- SUB 9
                     when "1001" => 
+                        --data_in <= codec_data_out - data_out;
                     -- NAND 10
                     when "1010" => 
+                        --data_in <= codec_data_out nand data_out;
                     -- SLT 11
                     when "1011" =>
+                        --data_in <= codec_data_out < data_out;
                     -- SHL 12
                     when "1100" =>
+                        --data_in <= codec_data_out sll data_out;
                     -- SHR 13 
-                    when "1101" => 
+                    when "1101" =>
+                        --data_in <= codec_data_out srl data_out; 
                     -- JEQ 14
                     when "1110" => 
+                        -- if(codec_data_out = data_out) then
+                            --IP <= Op3 + IP_aux
                     -- JMP 15
                     when "1111" => 
+                    -- IP_aux <= codec_data_out
+                    -- IP <= IP_aux
                 end case;
+            else
+                inner_halt <= '0';
+                IP_aux <= std_logic_vector(unsigned(0));
+                SP_aux <= std_logic_vector(unsigned(0));
+                IP <= IP_aux;
+                SP <= SP_aux;
             end if;
-        else 
-            wait on setup_finished;
+        --else 
+          --  wait on setup_finished;
         end if;
     end process;
 end architecture;
