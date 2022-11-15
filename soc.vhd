eec443b594eb48c5cf6dcd9bd8e866a62d0bee95
wildevn <45374307+wildevn@users.noreply.github.com>
2022-11-15 12:58:09 -0300

    Update soc.vhd

--- soc.vhd
+++ soc.vhd
@@ -29,21 +29,4 @@ begin
 
     codec_entity_firmware:   entity work.codec(bevahioral)
                              port map(codec_interrupt, codec_read, codec_write, codec_valid, codec_data_in, codec_data_out);
-    generic(
-        addr_width: natural := 16; -- Memory Address Width (in bits)
-        data_width: natural := 8 -- Data Width (in bits)
-    );
-
-entity codec is
-    port(
-        interrupt: in std_logic; -- Interrupt signal
-        read_signal: in std_logic; -- Read signal
-        write_signal: in std_logic; -- Write signal
-        valid: out std_logic; -- Valid signal
-
-    -- Byte written to codec
-        codec_data_in : in std_logic_vector(7 downto 0);
-    -- Byte read from codec
-        codec_data_out : out std_logic_vector(7 downto 0)
-    );
-end entity;
+end architecture;
